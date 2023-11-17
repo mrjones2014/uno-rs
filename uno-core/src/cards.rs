c81f3f1ebe213f15dce060fc9b78ccd98e01a4fe
Mat Jones <mat@mjones.network>
2023-11-17 13:43:33 -0500

    refactor

---
+++ uno-core/src/cards.rs
@@ -0,0 +1,124 @@
+use serde::{Deserialize, Serialize};
+use strum::{EnumIter, IntoEnumIterator};
+use thiserror::Error;
+
+#[derive(Debug, Eq, PartialEq, EnumIter, Clone, Copy, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum UnoValue {
+    Zero,
+    One,
+    Two,
+    Three,
+    Four,
+    Five,
+    Six,
+    Seven,
+    Eight,
+    Nine,
+    Skip,
+    Reverse,
+    Draw2,
+}
+
+#[derive(Debug, Eq, PartialEq, Clone, Copy, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum UnoWildCard {
+    Played { draw_4: bool, color: UnoColor },
+    Unplayed { draw_4: bool },
+}
+
+#[derive(Debug, Eq, PartialEq, EnumIter, Clone, Copy, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum UnoColor {
+    Red,
+    Blue,
+    Green,
+    Yellow,
+}
+
+#[derive(Debug, Eq, PartialEq, Clone, Copy, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub enum UnoCard {
+    Card { color: UnoColor, value: UnoValue },
+    Wild(UnoWildCard),
+}
+
+/// Errors describing why one card cannot be played on another.
+#[derive(Debug, Error)]
+pub enum UnoCardMatchError {
+    /// It's a normal card and the color and value both don't match
+    #[error("Card color and value both don't match.")]
+    NoMatch,
+    /// Wild card is in "unplayed" state (color not set)
+    #[error("Wild card is in unplayed state (color not set)")]
+    WildUnplayed,
+}
+
+impl UnoCard {
+    /// Returns `Ok(())` if the card is playable, otherwise
+    /// returns [`UnoPlayError`] describing why it cannot be played on
+    /// `other_card`.
+    ///
+    /// # Errors
+    ///
+    /// Returns an error describing why the card cannot be played. [`UnoCardMatchError`]
+    pub fn playable_on(&self, other_card: UnoCard) -> Result<(), UnoCardMatchError> {
+        match self {
+            UnoCard::Card {
+                color: self_color,
+                value: self_value,
+            } => match other_card {
+                UnoCard::Card {
+                    color: other_color,
+                    value: other_value,
+                } => {
+                    if self_color == &other_color || self_value == &other_value {
+                        Ok(())
+                    } else {
+                        Err(UnoCardMatchError::NoMatch)
+                    }
+                }
+                UnoCard::Wild(other_wild) => match other_wild {
+                    // can always play a wild card
+                    UnoWildCard::Played { .. } => Ok(()),
+                    UnoWildCard::Unplayed { .. } => Err(UnoCardMatchError::WildUnplayed),
+                },
+            },
+            UnoCard::Wild(wild) => match wild {
+                UnoWildCard::Played {
+                    color: self_color, ..
+                } => match other_card {
+                    UnoCard::Card {
+                        color: other_color, ..
+                    } => {
+                        // wild cards don't have a value, just a color
+                        if self_color == &other_color {
+                            Ok(())
+                        } else {
+                            Err(UnoCardMatchError::NoMatch)
+                        }
+                    }
+                    UnoCard::Wild(wild) => match wild {
+                        UnoWildCard::Played { .. } => Ok(()), // can always play a wild card
+                        UnoWildCard::Unplayed { .. } => Err(UnoCardMatchError::WildUnplayed),
+                    },
+                },
+                UnoWildCard::Unplayed { .. } => Err(UnoCardMatchError::WildUnplayed),
+            },
+        }
+    }
+
+    /// Get all permutations of `UnoCard::Card` by combining [`UnoColor`]s with [`UnoValue`]s.
+    /// Returns a list of [`UnoCard`]s representing one each of:
+    /// - All 4 colors, 0-9
+    /// - All 4 colors:
+    ///     - Skip
+    ///     - Reverse
+    ///     - Draw 2
+    #[must_use]
+    pub(crate) fn color_permutations() -> Vec<UnoCard> {
+        UnoColor::iter()
+            .flat_map(|color| UnoValue::iter().map(move |value| UnoCard::Card { color, value }))
+            .collect::<Vec<_>>()
+    }
+}
--- uno-core/src/lib.rs
+++ uno-core/src/lib.rs
@@ -1,252 +1,93 @@
 #![deny(clippy::all, clippy::pedantic, rust_2018_idioms, clippy::unwrap_used)]
 
+pub mod cards;
+
+use cards::{UnoCard, UnoValue, UnoWildCard};
 use rand::seq::SliceRandom;
+use serde::{Deserialize, Serialize};
 use std::{iter, usize};
-use strum::{EnumIter, IntoEnumIterator};
+use thiserror::Error;
 
 pub const FULL_DECK_SIZE: usize = 108;
-pub const USER_HAND_SIZE: usize = 108;
+pub const PLAYER_STARTING_HAND_SIZE: usize = 108;
 
-/// Numeric card
-#[derive(EnumIter, Clone, Copy)]
-pub enum UnoNumeric {
-    Zero,
-    One,
-    Two,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-}
-
-#[derive(EnumIter, Clone, Copy)]
-pub enum UnoSpecial {
-    Skip,
-    Reverse,
-    Draw2,
-}
-
-#[derive(Clone, Copy)]
-pub enum UnoValue {
-    Numeric(UnoNumeric),
-    Special(UnoSpecial),
-}
-
-#[derive(Clone, Copy)]
-pub enum UnoWild {
-    Wild,
-    WildDraw4,
-}
-
-#[derive(Clone, Copy)]
-pub enum UnoCard {
-    Red(UnoValue),
-    Blue(UnoValue),
-    Green(UnoValue),
-    Yellow(UnoValue),
-    Wild(UnoWild),
-}
-
-impl UnoCard {
-    /// Given the [`UnoValue`], generate all
-    /// Uno color cards for the value.
-    fn color_permutations(value: UnoValue) -> [UnoCard; 4] {
-        [
-            UnoCard::Red(value),
-            UnoCard::Yellow(value),
-            UnoCard::Blue(value),
-            UnoCard::Green(value),
-        ]
-    }
-}
-
-/// Collection of cards. Can be main deck or user hand.
-pub struct UnoDeck(Vec<UnoCard>);
-
+#[derive(Debug, Error)]
 pub enum UnoError {
     /// Not enough cards to deal to this many players
+    #[error("Too many players: max 4, attempted {0}")]
     TooManyPlayers(usize),
     /// Not enough cards left to deal
+    #[error("No cards left")]
     NoCardsLeft,
 }
 
+#[derive(Serialize, Deserialize)]
+pub struct UnoDeck(Vec<UnoCard>);
+
 impl UnoDeck {
-    /// Create a new full deck (all 108 cards). You can then deal the cards from this deck.
-    /// Returns the deck pre-shuffled.
+    /// Get a brand new, shuffled full Uno deck.
     #[must_use]
     pub fn new() -> Self {
-        let mut cards = Vec::with_capacity(FULL_DECK_SIZE);
+        let mut cards: Vec<UnoCard> = Vec::with_capacity(FULL_DECK_SIZE);
 
-        for i in 0..2 {
-            // two sets of numeric cards per color, but only one 0 card per color
-            for number in UnoNumeric::iter().skip(i) {
-                cards.append(&mut UnoCard::color_permutations(UnoValue::Numeric(number)).to_vec());
-            }
+        (0..2).for_each(|_| {
+            // 2 sets of 0-9s and special cards for each color, but only 1 zero card per color
+            // so: all the card permutations
+            cards.append(&mut UnoCard::color_permutations());
+            // then all the card permutations except the zero cards
+            cards.append(
+                &mut UnoCard::color_permutations()
+                    .into_iter()
+                    .filter(|card| {
+                        !matches!(
+                            card,
+                            UnoCard::Card {
+                                value: UnoValue::Zero,
+                                ..
+                            }
+                        )
+                    })
+                    .collect(),
+            );
+        });
 
-            // two of each special card per color
-            for special in UnoSpecial::iter() {
-                cards.append(&mut UnoCard::color_permutations(UnoValue::Special(special)).to_vec());
-            }
-        }
-
-        // 4 of each wild card type (plain wild or draw 4)
-        cards.append(&mut iter::repeat(UnoCard::Wild(UnoWild::Wild)).take(4).collect());
-        cards.append(
-            &mut iter::repeat(UnoCard::Wild(UnoWild::WildDraw4))
-                .take(4)
-                .collect(),
-        );
+        (0..8).for_each(|i| {
+            // then 4 each of wild and wild draw 4 cards
+            cards.push(UnoCard::Wild(UnoWildCard::Unplayed { draw_4: i < 4 }));
+        });
 
         cards.shuffle(&mut rand::thread_rng());
 
         UnoDeck(cards)
     }
 
-    pub fn pop(&mut self) -> Option<UnoCard> {
+    /// Draw a card from the deck (the last card in the deck is the "top").
+    /// Returns [`std::option::Option::None`] is there are no cards left.
+    pub fn draw_card(&mut self) -> Option<UnoCard> {
         self.0.pop()
     }
 
-    /// Deal a deck to the given number of players. Deals cards from the deck
-    /// and returns a `Vec<UnoDeck>` where the vector length is equal to the
-    /// number of given players (one deck per player).
-    /// Deals in place (main deck is mutated).
+    /// Deal out cards to specified number of players
     ///
     /// # Errors
     ///
-    /// This function will return an error if .
+    /// Returns an error if there are no cards left in the deck.
     pub fn deal(&mut self, players: usize) -> Result<Vec<UnoDeck>, UnoError> {
-        if players > 4 {
-            return Err(UnoError::TooManyPlayers(players));
+        let mut player_hands = iter::repeat(Vec::<UnoCard>::with_capacity(7))
+            .take(players)
+            .collect::<Vec<_>>();
+
+        for i in 0..(PLAYER_STARTING_HAND_SIZE * players) {
+            // deal cards round-robin style to each player, one at a time
+            player_hands[i % players].push(self.draw_card().ok_or(UnoError::NoCardsLeft)?);
         }
 
-        let mut player_decks: Vec<Vec<UnoCard>> = Vec::with_capacity(players);
-        // initialize vecs
-        (0..players).for_each(|i| {
-            player_decks[i] = Vec::with_capacity(USER_HAND_SIZE);
-        });
-
-        for _ in 0..USER_HAND_SIZE {
-            for j in 0..players {
-                let Some(player_deck) = player_decks.get_mut(j) else {
-                    unreachable!()
-                };
-
-                player_deck.push(self.pop().ok_or(UnoError::NoCardsLeft)?);
-            }
-        }
-
-        Ok(player_decks.into_iter().map(UnoDeck).collect())
-    }
-
-    /// Check if the given card can be played on top of the card on top of the current deck.
-    /// "Top" of the deck is the end of the `Vec<UnoCard>`.
-    pub fn can_play_card(&self, _next_card: UnoCard) {
-        unimplemented!("TODO")
+        Ok(player_hands.into_iter().map(UnoDeck).collect())
     }
 }
 
 impl Default for UnoDeck {
     fn default() -> Self {
         Self::new()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn new_creates_108_card_uno_deck() {
-        let deck = UnoDeck::new();
-        assert_eq!(FULL_DECK_SIZE, deck.0.len());
-
-        // 19 of each color numeric cards (set of 0-9 and set of 1-9)
-        assert_eq!(
-            19,
-            deck.0
-                .iter()
-                .filter(|card| matches!(card, UnoCard::Red(UnoValue::Numeric(_))))
-                .collect::<Vec<_>>()
-                .len()
-        );
-        assert_eq!(
-            19,
-            deck.0
-                .iter()
-                .filter(|card| matches!(card, UnoCard::Yellow(UnoValue::Numeric(_))))
-                .collect::<Vec<_>>()
-                .len()
-        );
-        assert_eq!(
-            19,
-            deck.0
-                .iter()
-                .filter(|card| matches!(card, UnoCard::Blue(UnoValue::Numeric(_))))
-                .collect::<Vec<_>>()
-                .len()
-        );
-        assert_eq!(
-            19,
-            deck.0
-                .iter()
-                .filter(|card| matches!(card, UnoCard::Green(UnoValue::Numeric(_))))
-                .collect::<Vec<_>>()
-                .len()
-        );
-
-        // 24 special cards (skip/reverse/draw 2), 6 of each color
-        assert_eq!(
-            6,
-            deck.0
-                .iter()
-                .filter(|card| matches!(card, UnoCard::Red(UnoValue::Special(_))))
-                .collect::<Vec<_>>()
-                .len()
-        );
-        assert_eq!(
-            6,
-            deck.0
-                .iter()
-                .filter(|card| matches!(card, UnoCard::Yellow(UnoValue::Special(_))))
-                .collect::<Vec<_>>()
-                .len()
-        );
-        assert_eq!(
-            6,
-            deck.0
-                .iter()
-                .filter(|card| matches!(card, UnoCard::Blue(UnoValue::Special(_))))
-                .collect::<Vec<_>>()
-                .len()
-        );
-        assert_eq!(
-            6,
-            deck.0
-                .iter()
-                .filter(|card| matches!(card, UnoCard::Green(UnoValue::Special(_))))
-                .collect::<Vec<_>>()
-                .len()
-        );
-
-        // 4 Wilds and 4 Wild Draw 4s
-        assert_eq!(
-            4,
-            deck.0
-                .iter()
-                .filter(|card| matches!(card, UnoCard::Wild(UnoWild::Wild)))
-                .collect::<Vec<_>>()
-                .len()
-        );
-        assert_eq!(
-            4,
-            deck.0
-                .iter()
-                .filter(|card| matches!(card, UnoCard::Wild(UnoWild::WildDraw4)))
-                .collect::<Vec<_>>()
-                .len()
-        );
     }
 }
